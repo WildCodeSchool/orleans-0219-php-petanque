6ddccdfcbf43fbc167ceaef5d3f93471f3ab55f2
Damien Rambourg <damien.rambourg@gmail.com>
2019-04-11 10:17:05 +0200

    Suppression du découpage de javascript dans un twig + Réinsertion Bootstrap JS dans layout

--- public/index.php
+++ public/index.php
@@ -7,6 +7,6 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-//require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
