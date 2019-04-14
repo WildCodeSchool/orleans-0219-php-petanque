de3ab013cb0ff3938ee64f2bfc1f074817aef6b3
AngelRionCervi <34657288+AngelRionCervi@users.noreply.github.com>
2019-04-14 15:06:41 +0200

    Fixed trigger issue and rearrange the code.

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -7,28 +7,24 @@ $(window).on('resize', () => {
 })
 
 $(window).scroll(() => {
-    let height = $(window).scrollTop()
-
-    if(height > 50) {
-        $('#navbarMain').removeClass('navbar-transp').addClass('navbar-colored')
-    }else{
-        $('#navbarMain').removeClass('navbar-colored').addClass('navbar-transp')
-    }
-
-});
-
+    checkWindowSize()
+})
 
 function checkWindowSize(){
 
     let width = $(window).width()
+    let scrollPos = $(window).scrollTop()
+    let widthBreakpoint = 975
+    let scrollTrigger = 50
 
-    if(width < 992){
+    if(width >= widthBreakpoint && scrollPos <= scrollTrigger){
+        $('#navbarNav > ul > li').addClass('nav-item-underline')
+        $('#navbarMain').removeClass('navbar-transp').addClass('navbar-transp')
+    }else{
         $('#navbarNav > ul > li').removeClass('nav-item-underline')
         $('#navbarMain').removeClass('navbar-transp').addClass('navbar-colored')
     }
-    if(width >= 992){
-        $('#navbarNav > ul > li').addClass('nav-item-underline')
-        $('#navbarMain').removeClass('navbar-transp').addClass('navbar-transp')
+    if(width < widthBreakpoint) {
+        $('#navbarMain').removeClass('navbar-transp').addClass('navbar-colored')
     }
-
 }
