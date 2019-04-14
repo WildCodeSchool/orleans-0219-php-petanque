61d985dfd5af71923d75cdaf2114e93d18b518ac
Damien Rambourg <damien.rambourg@gmail.com>
2019-04-14 15:47:39 +0200

    Ajout d'un Model EVentGenderManager pour la gestion de la table des mixité de compétiteurs

---
+++ src/Model/EventGenderManager.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class EventGenderManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'gendermix';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    /**
+     * @param array $item
+     * @return int
+     */
+    public function insert(array $item): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`descr`) VALUES (:name,:descr)");
+        $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
+        $statement->bindValue('descr', $item['descr'], \PDO::PARAM_STR);
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+
+
+    /**
+     * @param int $id
+     */
+    public function delete(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * @param array $item
+     * @return bool
+     */
+    public function update(array $item):bool
+    {
+
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE $this->table SET `name` = :name ,`descr` = :descr WHERE id=:id");
+        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
+        $statement->bindValue('descr', $item['descr'], \PDO::PARAM_STR);
+        return $statement->execute();
+    }
+}
