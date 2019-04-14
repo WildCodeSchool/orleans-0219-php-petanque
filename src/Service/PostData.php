c0d286c1b377352dc599d632a26f7a1e3e344a1c
Damien Rambourg <damien.rambourg@gmail.com>
2019-04-14 17:09:32 +0200

    Mise en place d'une classe et méthode de nettoyage générique pour el formulaire

---
+++ src/Service/PostData.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Service;
+
+
+class PostData
+{
+    private $postDatas;
+
+    /**
+     * PostData constructor.
+     * @param array $datas
+     */
+    public function __construct(array $datas)
+    {
+        $this->postDatas = $datas;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPostDatas(): array
+    {
+        return $this->postDatas;
+    }
+
+    /**
+     * @param array $postDatas
+     */
+    public function setPostDatas(array $postDatas): void
+    {
+        $this->postDatas = $postDatas;
+    }
+
+    /**
+     * Clean all data from postdata to an array
+     *
+     * @return array
+     */
+    public function cleanValues(): array
+    {
+        $dataArray=[];
+        foreach ($this->postDatas as $keyArray => $valueArray) {
+            $data= $valueArray;
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            $data = strip_tags($data);
+
+            $dataArray[$keyArray] = $data;
+        }
+
+        return $dataArray;
+    }
+}
