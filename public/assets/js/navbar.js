8ea7df19994ec0050b686ded35a389abcb225baa
Angel <a.rioncervi@gmail.com>
2019-04-16 14:25:05 +0200

    added box shadow

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -20,9 +20,11 @@ function checkWindowSize(){
     if(width >= widthBreakpoint && scrollPos <= scrollTrigger){
         $('#navbarNav > ul > li').addClass('nav-item-underline')
         $('#navbarMain').removeClass('navbar-transp').addClass('navbar-transp')
+        $('#navbarMain').removeClass('box_shadow_toggle')
     }else{
         $('#navbarMain').removeClass('navbar-transp').addClass('navbar-colored')
         $('#navbarNav > ul > li').addClass('nav-item-underline')
+        $('#navbarMain').addClass('box_shadow_toggle')
     }
     if(width < widthBreakpoint) {
         $('#navbarNav > ul > li').removeClass('nav-item-underline')
