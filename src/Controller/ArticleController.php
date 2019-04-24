3d56c67799208be0e6c6a982cc9a022a3ee41efc
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-04-24 09:59:20 +0200

    Mise en place du Controller et du Manager pour les articles

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\ArticleManager;
+
+/**
+ * Class ItemController
+ *
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * Display article listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $articleManager = new ArticleManager();
+        $articles = $articleManager->selectAll();
+
+        return $this->twig->render('Article/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+}
