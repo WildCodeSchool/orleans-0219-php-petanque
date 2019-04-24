ba15eb62d3a2005a89a640ce32beffbd1d39ec56
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-04-24 09:42:38 +0200

    Début intégration partie Vos Envies

---
+++ src/Controller/WishesController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\WishesManager;
+
+class WishesController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+
+
+
+        return $this->twig->render('Wishes/index.html.twig');
+    }
+}
