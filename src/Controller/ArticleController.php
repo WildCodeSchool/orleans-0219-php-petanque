b3a78f868e1dd6e032e8f81e2de9fd922e881c4e
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-04-25 14:07:10 +0200

    Fix gitignore et mise en place du Controller et Manager Article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+namespace App\Controller;
+use App\Model\ArticleManager;
+/**
+ * Class ItemController
+ *
+ */
+class ArticleController extends AbstractController
+{
+
+}
