ced3ab555970e59c37f8240f41052a6223730ccf
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-04-28 09:23:51 +0200

    Mise en place du Manager et de la sélection des photos d'un article

---
+++ src/Model/PictureArticleManager.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+namespace App\Model;
+
+/**
+ *
+ */
+class PictureArticleManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'picture';
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+    /**
+     * Retrieve all pictures from one article
+     *
+     * @param int $id
+     * @return array
+     */
+    public function selectPicturesFromArticleById(int $id) :array
+    {
+        $statement = "SELECT * FROM $this->table WHERE article_id=:id";
+        // prepared request
+        $statement = $this->pdo->prepare($statement);
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+}
