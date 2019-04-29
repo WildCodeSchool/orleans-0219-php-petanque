f242cba5c8d1ef9237bab6883fa978bd71aaa86a
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-04-29 15:09:12 +0200

    Simplification de l'upload de photos

--- src/Controller/AdminPictureArticleController.php
+++ src/Controller/AdminPictureArticleController.php
@@ -28,82 +28,66 @@ class AdminPictureArticleController extends AbstractController
      */
     public function showpicture(int $id)
     {
-        session_start();
-        $files = [];
-        $uploadedFiles=[];
-        if (isset($_SESSION['uploadfiles'])) {
-            $uploadedFiles=$_SESSION['uploadfiles'];
-            $_SESSION['uploadfiles'] = [];
-        }
+        $errorFiles = [];
         if (isset($_POST['submit'])) {
             $uploadDir = 'assets/images/article/';
             $allowedFormats = ['image/gif', 'image/jpeg', 'image/png',];
             $maxSize = 1000000;
+
             if (!empty($_FILES['upload'])) {
                 //Loop through each file
                 for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
-                    $shortNameFile = $_FILES['upload']['name'][$i];
                     if (!empty($_FILES['upload']['name'][$i])) {
                         if (!in_array($_FILES['upload']['type'][$i], $allowedFormats)) {
-                            $files[] = [
-                                'fileName' => $shortNameFile,
-                                'fileSize' => $_FILES['upload']['size'][$i],
-                                'fileType' => $_FILES['upload']['type'][$i],
-                                'uploaded' => false,
-                                'uploadederror' => 'Format non supporté.',
+                            $errorFiles[] = [
+                                'name' => $_FILES['upload']['name'][$i],
+                                'error' => 'Format de fichier non supporté. Formats autorisés : *.jpg, *.png, *.gif. ',
                             ];
-                        } elseif ($_FILES['upload']['size'][$i] > $maxSize) {
-                            $files[] = [
-                                'fileName' => $shortNameFile,
-                                'fileSize' => $_FILES['upload']['size'][$i],
-                                'fileType' => $_FILES['upload']['type'][$i],
-                                'uploaded' => false,
-                                'uploadederror' => 'Taille de fichier > à 1Mo.',
+                        };
+                        if ($_FILES['upload']['size'][$i] > $maxSize) {
+                            $errorFiles[] = [
+                                'name' => $_FILES['upload']['name'][$i],
+                                'error' => 'Taille du fichier trop importante.',
                             ];
-                        } else {
-                            //Get the temp file path
-                            $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
-                            //Make sure we have a filepath
-                            if (!empty($tmpFilePath)) {
-                                $extension = pathinfo($shortNameFile, PATHINFO_EXTENSION);
-                                $uploadFileName = 'pic' . uniqid();
-                                if (!empty($extension)) {
-                                    $uploadFileName .= '.' . $extension;
-                                }
-                                //save the url and the file
-                                $uploadFile = $uploadDir . $uploadFileName;
-                                //Upload the file into the temp dir
-                                if (move_uploaded_file($tmpFilePath, $uploadFile)) {
-                                    $files[] = [
-                                        'fileName' => $shortNameFile,
-                                        'fileSize' => $_FILES['upload']['size'][$i],
-                                        'fileType' => $_FILES['upload']['type'][$i],
-                                        'uploaded' => true,
-                                    ];
-                                    $pictureToInsert = [
-                                        'picture' => $uploadDir . $uploadFileName,
-                                        'article_id' => $id,
-                                    ];
-                                    $pictureArticle = new PictureArticleManager();
-                                    $pictureArticle->insert($pictureToInsert);
-                                }
-                            }
                         }
                     }
                 }
-                $_SESSION['uploadfiles'] = $files;
-                header('Location:/AdminPictureArticle/showpicture/' .$id);
-                exit();
+                if (empty($errorFiles)) {
+                    for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
+                        //Get the temp file path
+                        $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+                        $shortNameFile = $_FILES['upload']['name'][$i];
+                        $extension = pathinfo($shortNameFile, PATHINFO_EXTENSION);
+                        $uploadFileName = 'pic' . uniqid();
+
+                        if (!empty($extension)) {
+                            $uploadFileName .= '.' . $extension;
+                        }
+                        //save the url and the file
+                        $uploadFile = $uploadDir . $uploadFileName;
+                        //Upload the file into the temp dir
+                        if (move_uploaded_file($tmpFilePath, $uploadFile)) {
+                            $pictureToInsert = [
+                                'picture' => $uploadDir . $uploadFileName,
+                                'article_id' => $id,
+                            ];
+                            $pictureArticle = new PictureArticleManager();
+                            $pictureArticle->insert($pictureToInsert);
+                        }
+                    }
+                    header('Location:/AdminPictureArticle/showpicture/' . $id);
+                    exit();
+                }
             }
         }
-
         $pictureArticleManager = new PictureArticleManager();
         $pictures = $pictureArticleManager->selectPicturesFromArticleById($id);
         $article['id'] = $id;
         return $this->twig->render('Article/adminshowpicture.html.twig', [
             'pictures' => $pictures,
             'article' => $article,
-            'uploadedfiles' => $uploadedFiles,
+            'errors' => $errorFiles,
         ]);
     }
 }
